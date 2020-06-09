7aef99c7de5b002f06218e9d7417d0897d2a40f6
Jack Anderson <andersjack@gmail.com>
2020-06-08 21:13:40 -0600

    add in npc generator

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const _ = require('lodash');
 
+const { generateNpc } = require('./npcGenerator');
+
 const app = express();
 const port = 3000;
 
 app.use(cors());
 
 app.get('/npcs', function (req, res) {
-  res.send('nothing here!');
+  res.send(generateNpc());
 });
 
 app.get('/', (_, res) => res.send('Hello World!'));
