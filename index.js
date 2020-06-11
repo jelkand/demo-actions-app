b508d99fff9edd0b81f57d2ef722ce8306203a37
Jack Anderson <jelkand@users.noreply.github.com>
2020-06-11 09:21:48 -0600

    update port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const _ = require('lodash');
 const { generateNpc } = require('./npcGenerator');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
