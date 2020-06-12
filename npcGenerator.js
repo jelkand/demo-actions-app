fc97dd4899df8662eee75b07cecb8060ddf868af
Jack Anderson <jelkand@users.noreply.github.com>
2020-06-11 18:06:24 -0600

    Update npcGenerator.js

--- npcGenerator.js
+++ npcGenerator.js
@@ -7,7 +7,7 @@ const generateNpc = () => ({
   firstName: faker.name.firstName(),
   lastName: faker.name.lastName(),
   class: _.sample(classes),
-  level: _.random(1, 20),
+  // level: _.random(1, 20),
 });
 
 module.exports = {
